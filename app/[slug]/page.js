ad30c6dbb36d566fd5100823d8c29d3827300be4
mpower3-makler <hallo@mpower3.de>
2025-11-21 11:33:40 +0100

    Update page.js

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -1,14 +1,8 @@
 import ChatInterface from '../../components/ChatInterface'
-import { notFound } from 'next/navigation'
 
 export default function ChatPage({ params }) {
   const { slug } = params
 
-  // Optional: nur numerische Property-IDs zulassen
-  if (!/^[0-9]+$/.test(slug)) {
-    notFound()
-  }
-
+  // Keine Einschränkung mehr – jede Slug zeigt den Chat
   return <ChatInterface slug={slug} />
 }
-
